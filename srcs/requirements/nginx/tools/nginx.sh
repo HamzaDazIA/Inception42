570d5ccc71746e1cfd684a3402800060f6538003
HamzaDazIA <hamzadaziag@gmail.com>
2026-03-09 12:36:54 +0000

    push cnf of wordpress

---
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+set -e
+
+if [ ! -f /etc/.firstrun ]; then
+
+# generate ssl cetificate
+mkdir -p /etc/nginx/ssl/
+openssl req -x509 -nodes -days 365 -newkey rsa:4096  \
+            -out /etc/nginx/ssl/cert.crt \
+            -keyout /etc/nginx/ssl/cert.key \
+            -subj "/CN=${DOMAIN_NAME}" >/dev/null 2>/dev/null
+
+    touch /etc/.firstrun
+fi
+
+exec nginx -g 'daemon off;'
