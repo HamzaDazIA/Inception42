a82a1d8ee11c1ad335e0350888d89669025c4286
hdazia <hdazia@hamza.myguest.virtualbox.org>
2026-03-09 12:24:00 +0000

    push cnf of wordpress

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,11 +2,11 @@
 
 set -e 
 
-if [ ! -d "/var/lib/mysql/$MARIADB_DATABASE" ]; then
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
     echo "Start initialization Mariadb ... " 
 
-    [ -f "/run/secrets/mariadb_root_password" ] && export MYSQL_ROOT_PASSWORD=$(cat /run/secrets/mariadb_root_password)
-    [ -f "/run/secrets/mariadb_password" ] && export MYSQL_PASSWORD=$(cat /run/secrets/mariadb_password)
+    [ -f "/run/secrets/db_root_password" ] && export MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+    [ -f "/run/secrets/db_password" ] && export MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 
     mkdir -p /run/mysqld
     chown -R mysql:mysql /run/mysqld
@@ -16,9 +16,9 @@ if [ ! -d "/var/lib/mysql/$MARIADB_DATABASE" ]; then
     mariadbd --user=mysql --bootstrap << EOF
 
 FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS \`$MARIADB_DATABASE\`;
-CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON \`$MARIADB_DATABASE\`.* TO '$MARIADB_USER'@'%';
+CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY  '$MYSQL_ROOT_PASSWORD';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
---
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e 
+
+until mysqladmin ping -h "mariadb" --silent; do
+    echo "Waiting for MariaDB to be available..."
+    sleep 2
+done
+
+[ -f "/run/secrets/db_password" ] && export DB_PASSWORD=$(cat /run/secrets/db_password)
+[ -f "run/secrets/wordpress_admin_password" ] && export WP_ADMIN_PASSWORD=$(cat /run/secrets/wordpress_admin_password)
+[ -f "/run/secrets/db_root_password" ] && export DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+[ -f "run/secrets/wordpress_password" ] && export WP_PASSWORD=$(cat "run/secrets/wordpress_password")
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+    echo "Start initialization WordPress ... "
+
+    wp core download --path=/var/www/html --allow-root > /dev/null
+
+    wp config create --path=/var/www/html \
+        --dbname=$MYSQL_DATABASE \
+        --dbhost=mariadb:3306 \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$DB_PASSWORD \
+        --allow-root
+    
+    wp core install --path=/var/www/html --allow-root\
+        --skip-email \
+        --url=$DOMAIN_NAME \
+        --title="$WORDPRESS_TITLE" \
+        --admin_user=$WORDPRESS_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --email=$WORDPRESS_ADMIN_EMAIL
+
+    wp user creat --allow-root\
+        $WORDPRESS_USER \
+        $WORDPRESS_EMAIL \
+        --user_password= $WP_PASSWORD \
+        --role=author
+    
+    echo "WordPress step completed"
+else
+    echo "Wordpress is already initialaze"
+fi
+
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+echo "Starting PHP-FPM..."
+exec php-fpm8.2 -F
