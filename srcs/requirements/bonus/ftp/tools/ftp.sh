8e8e1da095834f0ed1beca2c694d7455123ee1c4
HamzaDazIA <hamzadaziag@gmail.com>
2026-03-11 10:45:33 +0000

    push same updates

--- srcs/requirements/bonus/ftp/tools/ftp.sh
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-if [ ! id "$FTP_USER" &>/dev/null ]; then
+if  ! id "$FTP_USER" &>/dev/null ; then
     useradd -d /var/www/hhml -s /usr/sbin/nologin $FTP_USER
-    [ -f /run/secrets/FTP_PASSWORD ] && export FTP_PASSWORD=$(cat /run/secrets/FTP_PASSWORD)
+
+    [ -f /run/secrets/FTP_PASSWORD ] && export FTP_PASSWORD=$(cat /run/secrets/ftp_password)
     
     echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
     usermod -aG www-data $FTP_USER
 
-    chown -R $FTP_USER:www-data /var/www/hhml
+    chown -R $FTP_USER:www-data /var/www/html
 
     echo "FTP user $FTP_USER created and configured."
 else 
