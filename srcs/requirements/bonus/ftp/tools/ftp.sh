0475a70235320f03620faf7279f68bfc7defa7b1
HamzaDazIA <hamzadaziag@gmail.com>
2026-03-11 10:39:56 +0000

    push same updates

---
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+if [ ! id "$FTP_USER" &>/dev/null ]; then
+    useradd -d /var/www/hhml -s /usr/sbin/nologin $FTP_USER
+    [ -f /run/secrets/FTP_PASSWORD ] && export FTP_PASSWORD=$(cat /run/secrets/FTP_PASSWORD)
+    
+    echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+
+    usermod -aG www-data $FTP_USER
+
+    chown -R $FTP_USER:www-data /var/www/hhml
+
+    echo "FTP user $FTP_USER created and configured."
+else 
+    echo "FTP user $FTP_USER already exists."
+fi
+
+exec vsftpd /etc/vsftpd.conf
